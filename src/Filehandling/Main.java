9755c1dff13479f379ceb800b4611d8132428f89
lakhan <anandparashar323@gmail.com>
2023-12-20 22:09:38 +0530

    file handling

---
+++ src/Filehandling/Main.java
@@ -0,0 +1,38 @@
+package Filehandling;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  throws IOException {
+//       code to     create a new file:
+        File myfile =new File("file.txt");
+//try {
+//    myfile.createNewFile();
+//}catch (IOException e){
+//    System.out.println("I am unable to read this file");
+//    e.printStackTrace();
+//    code to write a file
+    FileWriter writer=new FileWriter("file.txt");
+    writer.write("this is the first file of this course");
+    writer.close();
+//    code to read file
+        Scanner sc =new Scanner(myfile);
+        while ((sc.hasNextLine())){
+            String data =sc.nextLine();
+            System.out.println(data);
+        }
+        sc.close();
+//        code to delete a file
+        if(myfile.delete()){
+            System.out.println("I have delete this file "+myfile.getName());
+        }else {
+            System.out.println("some error occured ");
+        }
+
+}
+    }
+//}
